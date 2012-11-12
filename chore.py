6c57533fce3e01841a68e513b45b7ef4b49a2b64
Taesoo Kim <taesoo@mit.edu>
2012-11-12 02:16:20 -0500

    fix host path when diffing

--- chore.py
+++ chore.py
@@ -26,13 +26,22 @@ class ask_quit:
     def __call__(self, spn, hpn):
         exit(0)
 
+class ask_commitall:
+    desc = "C:commit(all)"
+    def __call__(self, spn, hpn):
+        # XXX. ignore signals
+        return True
+
 # interactively committing modified files to the host
 def interactive(box):
-    menu = [ask_diff(), ask_view(), ask_ignore(), ask_commit(), ask_quit()]
+
+    menu = [ask_diff(), ask_view(), ask_ignore(), ask_commit(),
+            ask_commitall(), ask_quit()]
+
     for root, dirs, files in os.walk(box.root):
         for name in files:
             spn = os.path.join(root, name)
-            hpn = spn.lstrip(box.root.rstrip("/"))
+            hpn = spn[len(box.root):]
 
             stop = False
             while not stop:
--- sandbox.py
+++ sandbox.py
@@ -20,49 +20,6 @@ from ptrace.error        import writeError
 from ptrace.func_call    import FunctionCallOptions
 from ptrace.ctypes_tools import formatAddress
 
-# interactively committing modified files to the host
-def interactive(box):
-    class ask_diff:
-        desc = "d:diff"
-        def __call__(self, spn, hpn):
-            os.system("diff -urN '%s' '%s'" % (hpn, spn))
-
-    class ask_view:
-        desc = "v:view"
-        def __call__(self, spn, hpn):
-            os.system("cat '%s'" % (spn))
-
-    class ask_ignore:
-        desc = "i:ignore"
-        def __call__(self, spn, hpn):
-            return True
-
-    class ask_commit:
-        desc = "c:commit"
-        def __call__(self, spn, hpn):
-            return True
-
-    class ask_quit:
-        desc = "q:quit"
-        def __call__(self, spn, hpn):
-            exit(0)
-
-    menu = [ask_diff(), ask_view(), ask_ignore(), ask_commit(), ask_quit()]
-    for root, dirs, files in os.walk(box.root):
-        for name in files:
-            spn = join(root, name)
-            hpn = spn.lstrip(box.root.rstrip("/"))
-
-            stop = False
-            while not stop:
-                print "> %s" % spn
-                print "  [?]" + ", ".join(m.desc for m in menu) + "> ",
-                c = kbhit()
-                print ""
-                for m in menu:
-                    if m.desc.startswith(c+":"):
-                        stop = m(spn, hpn)
-
 # main ptrace wrapper
 class Sandbox:
     def __init__(self, opts, args):

0e1cf4aa6afc908f919a26c50f6781e67200be93
Taesoo Kim <taesoo@mit.edu>
2012-11-16 11:07:20 -0800

    own ptrace

---
+++ process.py
@@ -0,0 +1,146 @@
+from ptrace import *
+    
+def run(args):
+    pid = os.fork()
+    
+    # child
+    if pid == 0:
+        ptrace_traceme()
+        os.execvp(args[0], args)
+        print "Failed to execute: %s" % " ".join(args)
+        exit(1)
+    # parent
+    else:
+        (pid, status) = os.wait()
+
+        # set to follow children
+        ptrace(PTRACE_SETOPTIONS, pid, 0,
+               PTRACE_O_TRACESYSGOOD    # SIGTRAP|0x80 if syscall call traps
+               | PTRACE_O_TRACEFORK     # PTRACE_EVENT_FORK
+               | PTRACE_O_TRACEVFORK    # PTRACE_EVENT_VFORK
+               | PTRACE_O_TRACECLONE    # PTRACE_EVENT_CLONE
+               | PTRACE_O_TRACEEXEC     # PTRACE_EVENT_EXEC
+               | PTRACE_O_TRACEEXIT)    # PTRACE_EVENT_EXIT
+
+        # interpose next syscall
+        ptrace_syscall(pid)
+        
+    return pid
+
+PS_ENTERING = 0
+PS_EXITING  = 1
+
+class process(object):
+    def __init__(self, pid):
+        self.pid = pid
+        self.state = PS_ENTERING
+        self.sc = None
+    
+    def syscall(self):
+        # new syscall
+        if self.sc is None or self.sc.exiting:
+            self.sc = syscall(self)
+        else:
+            self.sc.update()
+        return self.sc
+
+    def getregs(self):
+        return ptrace_getregs(self.pid)
+
+    def getreg(self, regname):
+        reg = self.getregs()
+        return getattr(reg, regname)
+
+    def setregs(self, regs):
+        return ptrace_setregs(self.pid, regs)
+
+    def setreg(self, regname, value):
+        regs = self.getregs()
+        setattr(regs, regname, value)
+        ptrace_setregs(self.pid, regs)
+
+    def peek(self, addr):
+        return ptrace_peek(self.pid, addr)
+
+    def poke(self, addr, word):
+        return ptrace_poke(self.pid, addr, word)
+    
+    def read_bytes(self, ptr, size):
+        data = b''
+        WORD = 8
+        offset = ptr % WORD
+        if offset:
+            # read word
+            ptr -= offset
+            blob = self.peek(ptr)
+
+            # read some bytes from the word
+            subsize = min(WORD - offset, size)
+            data = blob[offset:offset+subsize]
+
+            # move cursor
+            size -= subsize
+            ptr += WORD
+            
+        while size:
+            # read word
+            blob = self.peek(ptr)
+
+            # read bytes from the word
+            if size < WORD:
+                data += blob[:size]
+                break
+            data += blob
+
+            # move cursor
+            size -= WORD
+            ptr += WORD
+            
+        return data
+
+    def read_str(self, ptr, limit=1024):
+        rtn = []
+        WORD = 8
+        while len(rtn) < limit:
+            blob = self.peek(ptr)
+            null = blob.find(b'\0')
+            # done
+            if null != -1:
+                rtn.extend(blob[:null])
+                break
+            rtn.extend(blob)
+            ptr += WORD
+        return ''.join(rtn)
+
+    def write_bytes(self, ptr, blob):
+        # off
+        # [..bb]...[ee..]
+        #    ^        ^
+        #    +-- ptr  |
+        # [byte]      |
+        #             rear
+        #             
+        WORD = 8
+
+        # adjust front bytes
+        off = ptr % WORD
+        if off:
+            ptr  = ptr - off
+            byte = self.peek(ptr)
+            blob = byte[:off] + blob
+
+        # adjust rear bytes
+        rear = ptr + len(blob)
+        off = rear % WORD
+        if off:
+            byte = self.peek(rear - off)
+            blob = blob + byte[off:]
+
+        assert len(blob) % WORD == 0
+
+        # write
+        for i in range(0, len(blob), WORD):
+            self.poke(ptr + i, byte2word(blob[i:i+WORD]))
+
+    def write_str(self, ptr, blob):
+        self.write_bytes(ptr, blob + '\x00')
---
+++ tracer.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python2
+
+import os
+import sys
+import signal
+
+from ptrace   import *
+from syscall  import *
+
+def trace(args, handler):
+    pinfo = {}
+    pid = run(args)
+    pinfo[pid] = process(pid)
+
+    while len(pinfo) != 0:
+        (pid, status) = os.wait()
+
+        if not pid in pinfo:
+            print "> new pid: %s" % pid
+            pinfo[pid] = process(pid)
+
+        proc = pinfo[pid]
+        
+        if os.WIFEXITED(status):
+            code = os.WEXITSTATUS(status)
+            del pinfo[pid]
+            continue
+
+        if os.WIFSIGNALED(status):
+            code = os.WTERMSIG(status)
+            del pinfo[pid]
+            continue
+
+        sig = os.WSTOPSIG(status)
+        evt = status >> 16
+        if sig == signal.SIGTRAP and evt != 0:
+            print "> event: %s" % PTRACE_EVENTS[evt]
+
+        if evt == 0:
+            handler(proc, proc.syscall())
+            
+        ptrace_syscall(pid)
+
+if __name__ == '__main__':
+    # dump syscall
+    def handler(proc, sc):
+        print sc
+    
+    trace(sys.argv[1:], handler)

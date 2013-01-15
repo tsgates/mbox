5a86cd3a79f04f133d520d77bb0a0521d4788c9b
Taesoo Kim <taesoo@mit.edu>
2013-01-15 15:52:12 -0500

    add a test case for deleted files

---
+++ strace/tests-sbox/test-deletedfile.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -x
+#
+# pre: test -f tests-sbox/NOTE
+# post: test -f $HPWD/tests-sbox/NOTE
+# post: grep "No such file" $SPWD/err-rm
+# post: grep "No such file" $SPWD/err-head
+#
+
+# unlinking the existing file
+ls ./tests-sbox | grep NOTE
+rm ./tests-sbox/NOTE
+
+# no such a file
+rm ./tests-sbox/NOTE &> err-rm
+
+# no such a file
+head -1 ./tests-sbox/NOTE &> err-head

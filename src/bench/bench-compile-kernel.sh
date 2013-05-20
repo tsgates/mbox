27d8f766bcc2ec861ffef9215cfeeae4ac436913
Taesoo Kim <taesoo@mit.edu>
2013-05-20 18:05:33 -0400

    update kernel build with 3.8

--- src/bench/bench-compile-kernel.sh
+++ src/bench/bench-compile-kernel.sh
@@ -2,7 +2,7 @@
 
 JOB=${JOB:--j1}
 DIR=$(dirname "$0")/..
-KER=${KER:-/tmp/linux-git}
+KER=${KER:-/tmp/linux-3.8}
 
 if [ ! -e $KER ]; then
   echo "Can't find the $KER dir"
@@ -25,7 +25,11 @@ run() {
   echo "--------"
 }
 
-for i in `seq 1 5`; do
-  run make -C $KER $JOB kernel
-  run $DIR/mbox -i -s -- make -C $KER $JOB kernel
-done
+run make -C $KER $JOB kernel
+run $DIR/mbox -i -- make -C $KER $JOB kernel
+run $DIR/mbox -i -s -- make -C $KER $JOB kernel
+
+# for i in `seq 1 5`; do
+#   run make -C $KER $JOB kernel
+#   run $DIR/mbox -i -s -- make -C $KER $JOB kernel
+# done
